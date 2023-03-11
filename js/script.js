1e1bb5367435aac14541db558030af7ef23d1eb5
Lisa Anderson <lisachananderson@gmail.com>
2023-03-11 15:46:04 -0800

    init commit

---
+++ js/script.js
@@ -0,0 +1,14 @@
+window.addEventListener('load', () => {
+    const $ = (id) => document.getElementById(id);
+    let form = $('empForm');
+    let id, name, ext, email, department;
+    form.addEventListener('submit', (e) => {
+        e.preventDefault();
+        id = $('id').value;
+        name = $('name').value;
+        ext = $('ext').value;
+        email = $('email').value;
+        department = $('department').value;
+        console.log(`ID: ${id}\nName: ${name}\nExtension: ${ext}\nEmail: ${email}\nDepartment: ${department}`);
+    });
+});
